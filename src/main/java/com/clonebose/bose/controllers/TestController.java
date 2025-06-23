ecd66340e8eb35bf30fa12c9a8445dc2daf749ea
YJH <ybox92@gmail.com>
2025-06-23 12:14:55 +0900

    양지훈 페이지

---
+++ src/main/java/com/clonebose/bose/controllers/TestController.java
@@ -0,0 +1,7 @@
+package com.clonebose.bose.controllers;
+
+ public class TestController {
+        public static void main(String[] args) {
+            System.out.println("YYYYYYYYYJJJJJJJJJJJHHHHHHHHHH");
+        }
+}
