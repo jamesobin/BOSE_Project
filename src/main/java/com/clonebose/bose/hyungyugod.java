95419185d716a936c7163d23a3086ec8f404e3b7
Kwon OBin <jamesobin@naver.com>
2025-06-23 12:44:20 +0900

    [권오빈] 권오빈 수정

--- src/main/java/com/clonebose/bose/hyungyugod.java
+++ src/main/java/com/clonebose/bose/hyungyugod.java
@@ -3,9 +3,6 @@ package com.clonebose.bose;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.fasterxml.jackson.annotation.JsonCreator.Mode;
 
 
 @Controller
@@ -13,6 +10,12 @@ public class hyungyugod {
     @GetMapping("/hyungyugod")
     public String getHyungyuName(Model model) {
         model.addAttribute("name", "안녕하세요 성현규입니다.");
+        return "hyungyugod";
+    }
+
+    @GetMapping("/obin")
+    public String getObinName(Model model) {
+        model.addAttribute("name", "안녕하세요 권오빈입니다.");
         return "hyungyugod";
     }
     
